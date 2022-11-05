2cde237d388ebfdd2c9a56fbf20776dcafb0731c
Joshua <princejosh0007@gmail.com>
2022-11-05 15:41:43 +0100

    Revert "Cart"

--- App.tsx
+++ App.tsx
@@ -3,47 +3,22 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+import * as onboarding from './screens/onboarding';
 import {NativeBaseProvider} from 'native-base';
 
-import * as onboarding from './screens/onboarding';
-import * as ecommerce from './screens/ecommerce';
-import {store as cart} from './screens/ecommerce/cart';
-
-import {QueryClient, QueryClientProvider} from 'react-query';
-import {Provider} from 'react-redux';
-
-const Root = createNativeStackNavigator();
 const Stack = createNativeStackNavigator();
-
-const client = new QueryClient();
 
 export default function App() {
   return (
     <NativeBaseProvider>
       <NavigationContainer>
-        <Root.Navigator screenOptions={{headerShown: false}}>
-          <Root.Screen name={onboarding.name} component={onboarding.Scene} />
-
-          <Root.Screen name={ecommerce.name}>
-            {() => (
-              <QueryClientProvider client={client}>
-                <Provider store={cart}>
-                  <Stack.Navigator
-                    screenOptions={{
-                      headerShadowVisible: false,
-                      contentStyle: {backgroundColor: '#fff'},
-                    }}>
-                    <Stack.Screen
-                      name="root"
-                      component={ecommerce.Scene}
-                      options={{headerBackVisible: false}}
-                    />
-                  </Stack.Navigator>
-                </Provider>
-              </QueryClientProvider>
-            )}
-          </Root.Screen>
-        </Root.Navigator>
+        <Stack.Navigator screenOptions={{headerShadowVisible: false}}>
+          <Stack.Screen
+            name={onboarding.name}
+            component={onboarding.Scene}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
       </NavigationContainer>
     </NativeBaseProvider>
   );
--- screens/ecommerce/index.ts
+++
@@ -1,3 +0,0 @@
-export {Ecommerce as Scene} from './scene';
-
-export const name = 'ecommerce';
