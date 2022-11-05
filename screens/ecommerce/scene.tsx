534c762565687a06608c95aff1572742473879d9
Joshua Amaju <princejosh0007@gmail.com>
2022-11-05 12:42:50 +0100

    chore: add header

--- screens/ecommerce/scene.tsx
+++ screens/ecommerce/scene.tsx
@@ -1,3 +1,27 @@
+import {useNavigation} from '@react-navigation/native';
+import {Box, Text, VStack} from 'native-base';
+import {useLayoutEffect} from 'react';
+
 export function Ecommerce() {
+  const navigator = useNavigation();
+
+  useLayoutEffect(() => {
+    navigator.setOptions({
+      headerTitle: () => (
+        <VStack py={4}>
+          <Text bold fontSize="lg" color="gray.400">
+            Hi Joshua
+          </Text>
+          <Text bold fontSize="2xl">
+            Hungry Now? 🔥
+          </Text>
+        </VStack>
+      ),
+      headerRight: () => (
+        <Box w={10} h={10} bg="gray.400" borderRadius="full" />
+      ),
+    });
+  }, []);
+
   return <></>;
 }
