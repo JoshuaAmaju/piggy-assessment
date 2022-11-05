788e5dfa8a0f254302dd98d94e9c7eb398df1300
Joshua Amaju <princejosh0007@gmail.com>
2022-11-05 13:31:39 +0100

    feat: add categories

--- screens/ecommerce/scene.tsx
+++ screens/ecommerce/scene.tsx
@@ -1,18 +1,21 @@
+import React, {useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {
   Box,
   Button,
   Center,
   FlatList,
+  Image,
   Spinner,
   Text,
   View,
   VStack,
 } from 'native-base';
-import {useLayoutEffect} from 'react';
+import {useCallback, useLayoutEffect} from 'react';
 
 import axios from 'axios';
 import {useQuery} from 'react-query';
+import {ListRenderItem, StyleSheet, TouchableOpacity} from 'react-native';
 
 type Category = {
   idCategory: string;
@@ -21,18 +24,84 @@ type Category = {
   strCategoryDescription: string;
 };
 
+type Meal = {
+  idMeal: string;
+  strMeal: string;
+  strMealThumb: string;
+};
+
 async function getCategories() {
-  const res = await axios.get<Array<Category>>(
+  const res = await fetch(
     'https://www.themealdb.com/api/json/v1/1/categories.php',
   );
 
-  return res.data;
+  if (!res.ok) {
+    throw new Error('An error occurred while fetching categories');
+  }
+
+  const json = await res.json();
+
+  return (json as {categories: Array<Category>}).categories;
+  // return [];
+}
+
+async function getMealsByCategory(category: Category['strCategory']) {
+  // const res = await axios.get<{meals: Array<Meal>}>(
+  //   'https://www.themealdb.com/api/json/v1/1/filter.php',
+  //   {params: {c: category}},
+  // );
+  // return res.data.meals;
+}
+
+function Spacer() {
+  return <Box w={3} />;
 }
 
 export function Ecommerce() {
   const navigator = useNavigation();
 
-  const categories = useQuery(['categories'], getCategories);
+  const categories = useQuery(['categories'], getCategories, {
+    select: data => data.slice(0, 6),
+  });
+
+  console.log(categories.data);
+
+  const [selectedCategory, setSelectedCategory] =
+    useState<Category['strCategory']>();
+
+  const meals = useQuery(
+    ['meals', selectedCategory],
+    () => getMealsByCategory(selectedCategory!),
+    {enabled: !!selectedCategory},
+  );
+
+  const renderCategory = useCallback<ListRenderItem<Category>>(
+    ({item}) => {
+      const selected = selectedCategory === item.strCategory;
+
+      return (
+        <TouchableOpacity
+          onPress={() => setSelectedCategory(item.strCategory)}
+          style={[styles.category, selected && styles.selectedCategory]}>
+          <Image
+            style={styles.categoryThumb}
+            alt={item.strCategoryDescription}
+            source={{uri: item.strCategoryThumb}}
+          />
+
+          <Text
+            fontSize="md"
+            style={[
+              styles.categoryTitle,
+              selected && styles.selectedCategoryTitle,
+            ]}>
+            {item.strCategory}
+          </Text>
+        </TouchableOpacity>
+      );
+    },
+    [selectedCategory],
+  );
 
   useLayoutEffect(() => {
     navigator.setOptions({
@@ -50,12 +119,20 @@ export function Ecommerce() {
         <Box w={10} h={10} bg="gray.400" borderRadius="full" />
       ),
     });
-  }, []);
+  }, [navigator]);
 
   return (
     <View>
       {categories.data ? (
-        <FlatList data={categories.data} renderItem={() => <></>} />
+        <FlatList
+          horizontal
+          data={categories.data}
+          renderItem={renderCategory}
+          ItemSeparatorComponent={Spacer}
+          _contentContainerStyle={{px: '6'}}
+          keyExtractor={item => item.idCategory}
+          showsHorizontalScrollIndicator={false}
+        />
       ) : (
         <Center p={4}>
           {categories.isLoading ? (
@@ -71,3 +148,33 @@ export function Ecommerce() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  category: {
+    width: 90,
+    borderWidth: 1,
+    borderRadius: 20,
+    borderColor: '#ccc',
+    alignItems: 'center',
+    paddingVertical: 13,
+    paddingHorizontal: 10,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+  selectedCategory: {
+    borderWidth: 0,
+    backgroundColor: '#529F83',
+  },
+  categoryThumb: {
+    width: 30,
+    height: 30,
+  },
+  categoryTitle: {
+    marginTop: 5,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  selectedCategoryTitle: {
+    color: '#fff',
+  },
+});
