78df52ff138bff2127cec09cd5db2cd22bed3f9d
Joshua Amaju <princejosh0007@gmail.com>
2022-11-05 10:11:30 +0100

    chore: add ui theming

--- App.tsx
+++ App.tsx
@@ -3,14 +3,23 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+import * as onboarding from './screens/onboarding';
+import {NativeBaseProvider} from 'native-base';
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="dummy" component={() => <></>} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name={onboarding.name}
+            component={onboarding.Scene}
+            options={{headerTitleAlign: 'center'}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
